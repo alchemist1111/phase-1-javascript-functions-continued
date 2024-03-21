5eba577c5d92cc54031e2e72a953fc71a857dac1
eugene-emoyo-ojakaa <emoyoeugine@gmail.com>
2024-03-21 11:29:20 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const saturdayFun = function(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+
+  const wrapAdjective = function(delimiter = "*") {
+    return function(adjective) {
+      return `You are ${delimiter}${adjective}${delimiter}!`;
+    };
+  }
